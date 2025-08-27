e980249a4a8f919612f3366612004083dfd5e42f
masood <masoodacheampong@gmail.com>
2025-08-27 23:19:03 +0000

    Implement code changes to enhance functionality and improve performance

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,11 @@
-const { getDefaultConfig } = require("expo/metro-config")
+const { getDefaultConfig } = require("expo/metro-config");
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
-module.exports = config
+// Add this line to disable the experimental package exports feature.
+config.resolver.unstable_enablePackageExports = false;
+
+// An alternative fix for some versions is to add 'cjs' to the source extensions.
+config.resolver.sourceExts.push("cjs");
+
+module.exports = config;
